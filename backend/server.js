c1b4bdc2033ee1cb1515c5ae0ffbdea9a4495c8b
GPahuja67 <gaurikapahuja05@gmail.com>
2026-03-12 23:54:14 +0530

    modified server.js after adding authroutes

--- backend/server.js
+++ backend/server.js
@@ -6,26 +6,33 @@ const path = require("path");
 const expenseRoutes = require("./routes/expenseRoutes");
 const authRoutes = require("./routes/authRoutes");
 
-app.use("/api",authRoutes);
-const app = express();
+const app = express();   // ✅ create app first
 
+// middleware
 app.use(cors());
 app.use(express.json());
 
-// Serve frontend files
+// routes
+app.use("/api", expenseRoutes);
+app.use("/api", authRoutes);
+
+// serve frontend
 app.use(express.static(path.join(__dirname, "../frontend")));
 
+// database connection
 mongoose.connect("mongodb://127.0.0.1:27017/moneypilot")
 .then(()=>console.log("MongoDB Connected"))
 .catch(err=>console.log(err));
 
-app.use("/api", expenseRoutes);
-
-// Open dashboard on localhost
+// open login page first
 app.get("/", (req,res)=>{
-    res.sendFile(path.join(__dirname, "../frontend/pages/dashboard.html"));
+    res.sendFile(path.join(__dirname,"../frontend/pages/login.html"));
+});
+app.get("/register", (req,res)=>{
+res.sendFile(path.join(__dirname,"../frontend/pages/register.html"));
 });
 
+// start server
 app.listen(5000, ()=>{
     console.log("Server running on port 5000");
 });
