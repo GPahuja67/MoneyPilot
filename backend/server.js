0a8dc76f8381bacdc97f97f98e7201cde89d2143
GPahuja67 <gaurikapahuja05@gmail.com>
2026-03-13 10:30:04 +0530

    modified api call in server.js

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.get("/", (req,res)=>{
     res.sendFile(path.join(__dirname,"../frontend/pages/login.html"));
 });
 app.get("/register", (req,res)=>{
-res.sendFile(path.join(__dirname,"../frontend/pages/register.html"));
+  res.sendFile(path.join(__dirname,"../frontend/pages/register.html"));
 });
 
 // start server
